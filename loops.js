63c06ba256b32f75a033b5bcd3716cd834b03f58
Naenae L <oneprincesslea@gmail.com>
2018-11-20 19:06:20 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+/* for (var i = 1; i < 100; i++) {
+  console.log("Hello World the " + i + " time");
+}  /*  
+
+for (var i = 1; i < 26; i++) {
+  if (i = 1) {
+    console.log ("I am 1 strange loop"); 
+  } 
+  else {
+    console.log (`I am ${i} strange loops.`); 
+  }
+}; 
